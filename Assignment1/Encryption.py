d937d09724ac1d5fd1a1dd6c5f57a4d7316f4305
Kanskikuro <william.vo@live.no>
2024-09-12 01:50:42 +0200

    Finished to task 3.5

--- Assignment1/Encryption.py
+++ Assignment1/Encryption.py
@@ -1,15 +1,18 @@
 import math
 import random
 import string
+import time
 from caesarcipher import CaesarCipher
 
-#Values
-plainText = "Boang-Ny William Nguyen Vo Security and Vulnerability in Networks"
-
+#Inital Values
+plainText = "Hoang-Ny William Nguyen Vo Security and Vulnerability in Networks".lower().replace(" ", "")
 CaeserKey =	24
 NumericKey = 24513
 
-#tutorial on transposistion 
+
+#---------------Task1---------------------------------------------------------------------------------------------------------------------
+
+#tutorial on how to make a transposition cipher
 #https://www.geeksforgeeks.org/columnar-transposition-cipher/
 def transpositionCipher(key,textInput):
     #Calculates variables for matrix size from the key
@@ -18,10 +21,11 @@ def transpositionCipher(key,textInput):
     
     #Adding random letters to fill out the matrix
     textList= list(textInput)
-    fill_null = int((row * keyLength) - len(textInput))
-    for _ in range(fill_null):
+    remainder = int((row * keyLength) - len(textInput))
+    for _ in range(remainder):
         ranLetter = random.choice(string.ascii_lowercase)
-        textList.append("X")
+        #appends a letter the remainder amount of times to fill out the matrix
+        textList.append("x")
     
     #Create creating a matrix and inputting textInput
     matrix = [textList[i: i + keyLength]
@@ -40,6 +44,32 @@ def transpositionCipher(key,textInput):
     return cipher
 
 #OptionA = CaesarCipher(transpositionCipher(NumericKey,plainText), offset=24).encoded.lower().replace(" ", "")
-OptionB = transpositionCipher(NumericKey, CaesarCipher(plainText, offset=24).encoded).lower().replace(" ", "")
+OptionB = transpositionCipher(NumericKey, CaesarCipher(plainText, offset=24).encoded)
 
-print(OptionB)
+
+#--------------Task2---------------------------------------------------------------------------------------------------------
+plainTextFlipped = "Boang-Ny William Nguyen Vo Security and Vulnerability in Networks".lower().replace(" ", "")
+OptionBFlipped = transpositionCipher(NumericKey, CaesarCipher(plainTextFlipped, offset=24).encoded)
+
+#How to convert string to binary
+#https://stackoverflow.com/questions/18815820/how-to-convert-string-to-binary
+def binaryConv(text):
+    res = ''.join(format(ord(i), '08b') for i in text)
+    return res
+#How to count ones in binary and count ones in binary
+#https://www.geeksforgeeks.org/count-set-bits-using-python-list-comprehension/
+#https://stackoverflow.com/questions/19414093/how-to-xor-binary-with-python
+def binaryDiff(a,b):
+    a=binaryConv(a)
+    b=binaryConv(b)
+    
+    Xor = int(a,2) ^ int(b,2)
+    diffCount = '{0:b}'.format(Xor).count("1")
+    return (diffCount/len(a))*100
+
+#how to measure elapsed time
+#https://www.programiz.com/python-programming/examples/elapsed-time
+start = time.time()
+for _ in range(16):
+    plainTextFlipped = transpositionCipher(NumericKey, CaesarCipher(plainTextFlipped, offset=24).encoded).lower().replace(" ", "")
+    print( plainTextFlipped + ' ' + str(time.time()-start) + ' ' + str(binaryDiff(plainTextFlipped,OptionB)))
